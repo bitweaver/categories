9ddb9baebd3e535c8614f2c8d7484d4c1ef75553
Max Kremmel <xing@synapse.plus.com>
2005-08-03 12:19:25 +0000

    display categories menu only when user has permission to view it

--- bit_setup_inc.php
+++ bit_setup_inc.php
@@ -6,6 +6,8 @@
 		// creates the main categories object
 		require_once( CATEGORIES_PKG_PATH.'categ_lib.php' );
 		
-		$gBitSystem->registerAppMenu( 'categories', 'Categories', CATEGORIES_PKG_URL.'index.php', 'bitpackage:categories/menu_categories.tpl', 'categories');
+		if($gBitUser->hasPermission( 'bit_p_view_categories' ) ) {
+			$gBitSystem->registerAppMenu( 'categories', 'Categories', CATEGORIES_PKG_URL.'index.php', 'bitpackage:categories/menu_categories.tpl', 'categories');
+		}
 	}
 ?>
