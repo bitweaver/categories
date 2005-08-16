34085787285a6f4abf29696333261f5c1c9926f6
Christian Fowler <spider@viovio.com>
2005-08-16 06:12:38 +0000

    add catgories_nav to service registration

--- bit_setup_inc.php
+++ bit_setup_inc.php
@@ -10,6 +10,7 @@
 			'content_store_function' => 'categories_categorize',
 			'content_edit_tpl' => 'bitpackage:categories/categorize.tpl',
 			'content_view_tpl' => 'bitpackage:categories/categories_objects.tpl',
+			'content_nav_tpl' => 'bitpackage:categories/categories_nav.tpl',
 		) );
 		// creates the main categories object
 		require_once( CATEGORIES_PKG_PATH.'categ_lib.php' );
