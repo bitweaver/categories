ac78d5405f5aa9be04165839c8676515e7fd84bb
Stephan Borg <wolff_borg@users.sourceforge.net>
2005-10-03 07:45:22 +0000

    Fixed Whats Related module

--- modules/mod_whats_related.php
+++ modules/mod_whats_related.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * $Header: /cvsroot/bitweaver/_bit_categories/modules/mod_whats_related.php,v 1.1.1.1.2.4 2005/10/03 07:44:04 wolff_borg Exp $
+ * $Header: /cvsroot/bitweaver/_bit_categories/modules/mod_whats_related.php,v 1.1.1.1.2.5 2005/10/03 07:45:22 wolff_borg Exp $
  *
  * Copyright (c) 2004 bitweaver.org
  * Copyright (c) 2003 tikwiki.org
@@ -8,7 +8,7 @@
  * All Rights Reserved. See copyright.txt for details and a complete list of authors.
  * Licensed under the GNU LESSER GENERAL PUBLIC LICENSE. See license.txt for details
  *
- * $Id: mod_whats_related.php,v 1.1.1.1.2.4 2005/10/03 07:44:04 wolff_borg Exp $
+ * $Id: mod_whats_related.php,v 1.1.1.1.2.5 2005/10/03 07:45:22 wolff_borg Exp $
  * @package categories
  * @subpackage modules
  */
@@ -16,16 +16,18 @@ global $gQueryUserId, $module_rows, $module_params, $categlib, $gContent;
 /**
  * required setup
  */
-require_once( CATEGORIES_PKG_PATH.'categ_lib.php');
+if (isset($gContent)) {
+	require_once( CATEGORIES_PKG_PATH.'categ_lib.php');
 
-$categories = $categlib->get_object_categories( $gContent->getContentType(), $gContent->getContentId() );
-foreach($categories as $cat)
-	$cats[] = $cat["category_id"];
+	$categories = $categlib->get_object_categories( $gContent->getContentType(), $gContent->getContentId() );
+	foreach($categories as $cat)
+		$cats[] = $cat["category_id"];
 
-$content = $categlib->get_related($cats, $module_rows);
-foreach($content as $con) {
-	$tmp = $gContent->getLibertyObject( $con );
-	$whatsRelated[] = $tmp->getDisplayLink($tmp->getTitle(), $tmp->mInfo);
+	$content = $categlib->get_related($cats, $module_rows);
+	foreach($content as $con) {
+		$tmp = $gContent->getLibertyObject( $con );
+		$whatsRelated[] = $tmp->getDisplayLink($tmp->getTitle(), $tmp->mInfo);
+	}
+	$gBitSmarty->assign_by_ref('whatsRelated', $whatsRelated);
 }
-$gBitSmarty->assign_by_ref('whatsRelated', $whatsRelated);
 ?>
--- modules/mod_whats_related.tpl
+++ modules/mod_whats_related.tpl
@@ -1,20 +1,12 @@
-{* $Header: /cvsroot/bitweaver/_bit_categories/modules/mod_whats_related.tpl,v 1.1.1.1.2.1 2005/10/03 05:20:46 wolff_borg Exp $ *}
+{* $Header: /cvsroot/bitweaver/_bit_categories/modules/mod_whats_related.tpl,v 1.1.1.1.2.2 2005/10/03 07:44:04 wolff_borg Exp $ *}
 {strip}
 {bitmodule title="$moduleTitle" name="whats_related"}
-	<ol>
+	<ul>
 		{section name=ix loop=$whatsRelated}
 			<li>
-				<a href="{$whatsRelated[ix].display_url}" title="{$whatsRelated[ix].title} - {$whatsRelated[ix].last_modified|bit_short_datetime}, by {displayname user=$whatsRelated[ix].modifier_user real_name=$whatsRelated[ix].modifier_real_name nolink=1}{if (strlen($whatsRelated[ix].title) > $maxlen) AND ($maxlen > 0)}, {$whatsRelated[ix].title}{/if}">
-					{if $maxlen gt 0}
-						{$whatsRelated[ix].title|truncate:$maxlen:"...":true}
-					{else}
-						{$whatsRelated[ix].title}
-					{/if}
-				</a>
+				{$whatsRelated[ix]}
 			</li>
-		{sectionelse}
-			<li></li>
 		{/section}
-	</ol>
+	</ul>
 {/bitmodule}
 {/strip}
