924362b702ffda6af743d409081ea5f8969735b5
Stephan Borg <wolff_borg@users.sourceforge.net>
2005-12-07 06:24:42 +0000

    Fixed uninitialized array

--- modules/mod_whats_related.php
+++ modules/mod_whats_related.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * $Header: /cvsroot/bitweaver/_bit_categories/modules/mod_whats_related.php,v 1.1.1.1.2.5 2005/10/03 07:45:22 wolff_borg Exp $
+ * $Header: /cvsroot/bitweaver/_bit_categories/modules/mod_whats_related.php,v 1.1.1.1.2.6 2005/12/07 06:24:42 wolff_borg Exp $
  *
  * Copyright (c) 2004 bitweaver.org
  * Copyright (c) 2003 tikwiki.org
@@ -8,7 +8,7 @@
  * All Rights Reserved. See copyright.txt for details and a complete list of authors.
  * Licensed under the GNU LESSER GENERAL PUBLIC LICENSE. See license.txt for details
  *
- * $Id: mod_whats_related.php,v 1.1.1.1.2.5 2005/10/03 07:45:22 wolff_borg Exp $
+ * $Id: mod_whats_related.php,v 1.1.1.1.2.6 2005/12/07 06:24:42 wolff_borg Exp $
  * @package categories
  * @subpackage modules
  */
@@ -20,6 +20,7 @@ if (isset($gContent)) {
 	require_once( CATEGORIES_PKG_PATH.'categ_lib.php');
 
 	$categories = $categlib->get_object_categories( $gContent->getContentType(), $gContent->getContentId() );
+	$cats = array();
 	foreach($categories as $cat)
 		$cats[] = $cat["category_id"];
 
