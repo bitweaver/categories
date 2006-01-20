47cfe4e0ee9f3b6ff351b5f6b0d44257250fcfb4
Max Kremmel <xing@synapse.plus.com>
2006-01-20 11:07:13 +0000

    merge recent changes into HEAD

--- bit_setup_inc.php
+++ bit_setup_inc.php
@@ -18,7 +18,7 @@
 		require_once( CATEGORIES_PKG_PATH.'categ_lib.php' );
 
 		if($gBitUser->hasPermission( 'bit_p_view_categories' ) ) {
-			$gBitSystem->registerAppMenu( CATEGORIES_PKG_DIR, 'Categories', CATEGORIES_PKG_URL.'index.php', 'bitpackage:categories/menu_categories.tpl', CATEGORIES_PKG_NAME );
+			$gBitSystem->registerAppMenu( CATEGORIES_PKG_NAME, ucfirst( CATEGORIES_PKG_DIR ), CATEGORIES_PKG_URL.'index.php', 'bitpackage:categories/menu_categories.tpl', CATEGORIES_PKG_NAME );
 		}
 	}
 ?>
