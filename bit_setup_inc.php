cf25c0a21dcb2d1547456083ebec9b94cd3d5799
Max Kremmel <xing@synapse.plus.com>
2006-02-17 23:51:40 +0000

    registerPackage now takes a hash instead of multiple parameters - keeps # of parameters under control

--- bit_setup_inc.php
+++ bit_setup_inc.php
@@ -1,24 +1,30 @@
 <?php
-	global $gBitSystem;
-	$gBitSystem->registerPackage( 'categories', dirname( __FILE__).'/', TRUE, LIBERTY_SERVICE_CATEGORIZATION );
+global $gBitSystem;
 
-	if( $gBitSystem->isPackageActive( 'categories' ) ) {
-		$gLibertySystem->registerService( LIBERTY_SERVICE_CATEGORIZATION, CATEGORIES_PKG_NAME, array(
-			'content_display_function' => 'categories_display',
-			'content_edit_function' => 'categories_object_edit',
-			'content_expunge_function' => 'categories_object_expunge',
-			'content_preview_function' => 'categories_object_edit',
-			'content_store_function' => 'categories_categorize',
-			'content_edit_tab_tpl' => 'bitpackage:categories/categorize.tpl',
-			'content_view_tpl' => 'bitpackage:categories/categories_objects.tpl',
-			'content_nav_tpl' => 'bitpackage:categories/categories_nav.tpl',
-		) );
+$registerHash = array(
+	'package_name' => 'categories',
+	'package_path' => dirname( __FILE__ ).'/',
+	'service' => LIBERTY_SERVICE_CATEGORIZATION,
+);
+$gBitSystem->registerPackage( $registerHash );
 
-		// creates the main categories object
-		require_once( CATEGORIES_PKG_PATH.'categ_lib.php' );
+if( $gBitSystem->isPackageActive( 'categories' ) ) {
+	$gLibertySystem->registerService( LIBERTY_SERVICE_CATEGORIZATION, CATEGORIES_PKG_NAME, array(
+		'content_display_function' => 'categories_display',
+		'content_edit_function' => 'categories_object_edit',
+		'content_expunge_function' => 'categories_object_expunge',
+		'content_preview_function' => 'categories_object_edit',
+		'content_store_function' => 'categories_categorize',
+		'content_edit_tab_tpl' => 'bitpackage:categories/categorize.tpl',
+		'content_view_tpl' => 'bitpackage:categories/categories_objects.tpl',
+		'content_nav_tpl' => 'bitpackage:categories/categories_nav.tpl',
+	) );
 
-		if($gBitUser->hasPermission( 'bit_p_view_categories' ) ) {
-			$gBitSystem->registerAppMenu( CATEGORIES_PKG_NAME, ucfirst( CATEGORIES_PKG_DIR ), CATEGORIES_PKG_URL.'index.php', 'bitpackage:categories/menu_categories.tpl', CATEGORIES_PKG_NAME );
-		}
+	// creates the main categories object
+	require_once( CATEGORIES_PKG_PATH.'categ_lib.php' );
+
+	if($gBitUser->hasPermission( 'bit_p_view_categories' ) ) {
+		$gBitSystem->registerAppMenu( CATEGORIES_PKG_NAME, ucfirst( CATEGORIES_PKG_DIR ), CATEGORIES_PKG_URL.'index.php', 'bitpackage:categories/menu_categories.tpl', CATEGORIES_PKG_NAME );
 	}
+}
 ?>
