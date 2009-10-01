ef738ed93fa8ee3bc02c70d55745c76fca130be0
wjames5 <will@tekimaki.com>
2009-10-01 14:16:59 +0000

    remove ref to non-existant copyright.txt file

--- admin/index.php
+++ admin/index.php
@@ -1,13 +1,13 @@
 <?php
 
-// $Header: /cvsroot/bitweaver/_bit_categories/admin/index.php,v 1.10 2009/10/01 13:45:33 wjames5 Exp $
+// $Header: /cvsroot/bitweaver/_bit_categories/admin/index.php,v 1.11 2009/10/01 14:16:59 wjames5 Exp $
 
 // Copyright (c) 2002-2003, Luis Argerich, Garland Foster, Eduardo Polidor, et. al.
-// All Rights Reserved. See copyright.txt for details and a complete list of authors.
+// All Rights Reserved. See below for details and a complete list of authors.
 // Licensed under the GNU LESSER GENERAL PUBLIC LICENSE. See http://www.gnu.org/copyleft/lesser.html for details.
 
 //
-// $Header: /cvsroot/bitweaver/_bit_categories/admin/index.php,v 1.10 2009/10/01 13:45:33 wjames5 Exp $
+// $Header: /cvsroot/bitweaver/_bit_categories/admin/index.php,v 1.11 2009/10/01 14:16:59 wjames5 Exp $
 //
 
 // Initialization
